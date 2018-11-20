b8e7df74839af6f8b47c68f123516ed8c4e47be0
Olexander <olexander.sj@gmail.com>
2018-11-20 23:08:18 +0200

    Removing GitHub token

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ window.onload = function(){
 	data();
 }
 
-const access_token = 'b3d3a5f9f1228d360c47b6b13fa4dc6c2cbbece1';
+const access_token = '?access_token=' + 'b3d3a5f9f1228d360c47b6b13fa4dc6c2cbbece1';
 
 let data = () => {
-    fetch(`https://api.github.com/repos/kottans/frontend-2019-homeworks/contents/submissions/?access_token=${access_token}`).then( x => { 
+    fetch(`https://api.github.com/repos/kottans/frontend-2019-homeworks/contents/submissions`).then( x => { 
    			return x.json();
 		}).then( data => {
 			console.log(data);
