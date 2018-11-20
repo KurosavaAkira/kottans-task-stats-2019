60908b5e3c0d948725f4691b82331fad7a203470
Olexander <olexander.sj@gmail.com>
2018-11-20 22:38:46 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Kottans task stats 2019
+
+Statistics of complited tasks from [Kottans Front-End Course 2019](https://github.com/kottans/frontend)
+
+Repository for homework from which statistics are generated is located [here](https://github.com/kottans/frontend-2019-homeworks)
+
+## [View statistics](https://KurosavaAkira.github.io/kottans-task-stats-2019/)
---
+++ index.js
@@ -0,0 +1,17 @@
+window.onload = function(){
+	data();
+}
+
+const access_token = 'b3d3a5f9f1228d360c47b6b13fa4dc6c2cbbece1';
+
+let data = () => {
+    fetch(`https://api.github.com/repos/kottans/frontend-2019-homeworks/contents/submissions/?access_token=${access_token}`).then( x => { 
+   			return x.json();
+		}).then( data => {
+			console.log(data);
+		}).catch(function(err) {  
+    		console.log('Fetch Error :', err); 
+  		});
+}
+
+//https://api.github.com/repos/kottans/frontend-2019-homeworks/contents/submissions/KurosavaAkira?ref=master
