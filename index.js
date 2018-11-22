227c74b37b8cdda47d4a3dad193a74a107176ab5
Olexander <olexander.sj@gmail.com>
2018-11-22 23:31:42 +0200

    adding Student class and get_students function

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
-window.onload = function(){
-	data();
+window.onload = function() {
+	createStudents();
 }
 
-const access_token = '?access_token=' + 'b3d3a5f9f1228d360c47b6b13fa4dc6c2cbbece1';
+//const access_token = 'b3d3a5f9f1228d360c47b6b13fa4dc6c2cbbece1';
+const access_token = '';
+const github_api_get_students = `https://api.github.com/repos/kottans/frontend-2019-homeworks/contents/submissions/?access_token=${access_token}`;
+const github_api_get_student_tasks = `https://api.github.com/repos/kottans/frontend-2019-homeworks/contents/submissions/KurosavaAkira?ref=master`;
 
-let data = () => {
-    fetch(`https://api.github.com/repos/kottans/frontend-2019-homeworks/contents/submissions`).then( x => { 
-   			return x.json();
-		}).then( data => {
-			console.log(data);
-		}).catch(function(err) {  
-    		console.log('Fetch Error :', err); 
-  		});
+const get_students = async () => {
+	try {
+		const response = await fetch(github_api_get_students);
+		if (!response.ok) throw new Error(response.statusText);
+		const data = await response.json();
+		return data;
+	} catch(err) {
+		return err;
+	}
 }
 
-//https://api.github.com/repos/kottans/frontend-2019-homeworks/contents/submissions/KurosavaAkira?ref=master
+class Student {
+	constructor(name, tasks, tasks_url, tasks_api_url) {
+		this.name = name;
+		this.tasks = tasks;
+		this.tasks_url = tasks_url;
+		this.tasks_api_url = tasks_api_url;
+	}
+}
+
+const createStudents = async () => {
+	const row_students = await get_students();
+	let students = [];
+	row_students.forEach(student => {
+		let new_student = new Student(student.name, [], student.html_url, student.git_url);
+		students.push(new_student);
+	});
+	return console.log(students);
+}
